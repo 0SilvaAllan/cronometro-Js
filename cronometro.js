c99a7b3d7f074b6dc271d8d12b58f5ae4adf33f5
Allan da silva <95647022+0SilvaAllan@users.noreply.github.com>
2024-06-15 17:23:42 -0300

    Update cronometro.js

--- cronometro.js
+++ cronometro.js
@@ -1,51 +1,41 @@
-var countDownData = new Data("Out20, 2021 14:00.00").getTime();
+var countDownDate = new Date("Oct 20, 2021 14:00:00").getTime();
 
-    var x =setInterval(function () {
-        var now = new Data().getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
 
-        var distance = countDownData - now;
+    var distance = countDownDate - now;
 
-        var dia = Math.floor(distance / (1000 * 60 * 60 * 24))
-var horas = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-var minutos = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-var segundos = Math.floor((distance % (1000 * 60)) / 1000);
+    var dias = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var horas = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutos = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var segundos = Math.floor((distance % (1000 * 60)) / 1000);
 
-        documentgetElementByld("dias").innerHTML = dias;
-        documentgetElementByld("horas").innerHTML = horas;
-        documentgetElementByld("minutos").innerHTML = minutos;
-        documentgetElementByld("segundos").innerHTML = segundos;
+    document.getElementById("dias").innerHTML = dias;
+    document.getElementById("horas").innerHTML = horas;
+    document.getElementById("minutos").innerHTML = minutos;
+    document.getElementById("segundos").innerHTML = segundos;
 
-        effectDCircle(dias, horas, minutos, segundos);
-  
+    effectDCircle(dias, horas, minutos, segundos);
+
 }, 1000);
 
-effectDCircle = function (d, h, m, s) {
+function effectDCircle(d, h, m, s) {
+    const strokeDashoffset = 439;
+    var circleSVG = document.getElementsByClassName('outer');
 
-    const max_seg = 60;
-    const max_min = 60;
-    const max_hour = 24;
-    const max_day = 30;
+    var valPerDay = strokeDashoffset / 30; // Max 30 dias
+    var sizeDay = strokeDashoffset - (valPerDay * d);
+    circleSVG[0].style.strokeDashoffset = sizeDay;
 
+    var valPerHour = strokeDashoffset / 24; // Max 24 horas
+    var sizeHour = strokeDashoffset - (valPerHour * h);
+    circleSVG[1].style.strokeDashoffset = sizeHour;
 
-    const strokeDashoffset=439;
+    var valPerMin = strokeDashoffset / 60; // Max 60 minutos
+    var sizeMin = strokeDashoffset - (valPerMin * m);
+    circleSVG[2].style.strokeDashoffset = sizeMin;
 
-    var circleSVG=DocumentgetElementsByClassName('outer');
-
-    var valPerDay=strokeDashoffset/max_day;
-    var size=strokeDashoffset-(valPerDay*d);
-    circleSVG[0].style.strokeDashoffset=size;
-
-    var valPerDay=strokeDashoffset/max_hour;
-    var size=strokeDashoffset-(valPerHour*h);
-    circleSVG[1].style.strokeDashoffset=size;
-
-    var valPerDay=strokeDashoffset/max_min;
-    var size=strokeDashoffset-(valPerMin*m);
-    circleSVG[2].style.strokeDashoffset=size;
-
-    var valPerDay=strokeDashoffset/max_sec;
-    var size=strokeDashoffset-(valPerSecond*s);
-    circleSVG[3].style.strokeDashoffset=size;
-    
-
+    var valPerSec = strokeDashoffset / 60; // Max 60 segundos
+    var sizeSec = strokeDashoffset - (valPerSec * s);
+    circleSVG[3].style.strokeDashoffset = sizeSec;
 }
